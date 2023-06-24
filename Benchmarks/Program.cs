b7a4119d0780d9be6728af363484db64d8212b74
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-06-24 23:36:16 +0300

    Add benchmarks for StringExtensions bytes ToString

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+class Program {
+	static void Main (string [] args)
+	{
+		var switcher = BenchmarkSwitcher.FromAssembly (typeof (Program).Assembly);
+		if (args.Length > 0) {
+			switcher.Run (args);
+		} else {
+			switcher.RunAll ();
+		}
+	}
+}
