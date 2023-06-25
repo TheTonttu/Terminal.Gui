83be22cadea763d9fc582a5c54e61c51d5f44b5e
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-06-25 09:53:24 +0300

    Add list version of BytesToString IEnumerableToArray and add repetition parameter to the BytesToString benchmark

--- Benchmarks/StringExtensions/BytesToString.cs
+++ Benchmarks/StringExtensions/BytesToString.cs
@@ -6,9 +6,32 @@ namespace Benchmarks.StringExtensions {
 	[MemoryDiagnoser]
 	public class BytesToString {
 
+		[Params (1, 100, 10_000)]
+		public int Repetitions { get; set; }
+
 		[Benchmark (Baseline = true)]
 		[ArgumentsSource (nameof (ArrayDataSource))]
-		public string IEnumerableToArray (IEnumerable<byte> bytes, Encoding? encoding = null)
+		public string IEnumerableToArray_Array (IEnumerable<byte> bytes, Encoding? encoding = null)
+		{
+			string str = string.Empty;
+			for (int i = 0; i < Repetitions; i++) {
+				str = IEnumerableToArrayInternal (bytes, ref encoding);
+			}
+			return str;
+		}
+
+		[Benchmark]
+		[ArgumentsSource (nameof (ListDataSource))]
+		public string IEnumerableToArray_List (IEnumerable<byte> bytes, Encoding? encoding = null)
+		{
+			string str = string.Empty;
+			for (int i = 0; i < Repetitions; i++) {
+				str = IEnumerableToArrayInternal (bytes, ref encoding);
+			}
+			return str;
+		}
+
+		private static string IEnumerableToArrayInternal (IEnumerable<byte> bytes, ref Encoding? encoding)
 		{
 			if (encoding == null) {
 				encoding = Encoding.UTF8;
@@ -18,16 +41,24 @@ namespace Benchmarks.StringExtensions {
 
 		[Benchmark]
 		[ArgumentsSource (nameof (ArrayDataSource))]
-		public string Array_ReadOnlySpan (byte [] bytes, Encoding? encoding = null)
+		public string ReadOnlySpan_Array (byte [] bytes, Encoding? encoding = null)
 		{
-			return ReadOnlySpanInternal (bytes.AsSpan (), encoding);
+			string str = string.Empty;
+			for (int i = 0; i < Repetitions; i++) {
+				str = ReadOnlySpanInternal (bytes.AsSpan (), encoding);
+			}
+			return str;
 		}
 
 		[Benchmark]
 		[ArgumentsSource (nameof (ListDataSource))]
-		public string List_ReadOnlySpan (List<byte> bytes, Encoding? encoding = null)
+		public string ReadOnlySpan_List (List<byte> bytes, Encoding? encoding = null)
 		{
-			return ReadOnlySpanInternal (CollectionsMarshal.AsSpan (bytes), encoding);
+			string str = string.Empty;
+			for (int i = 0; i < Repetitions; i++) {
+				str = ReadOnlySpanInternal (CollectionsMarshal.AsSpan (bytes), encoding);
+			}
+			return str;
 		}
 
 		private static string ReadOnlySpanInternal (in ReadOnlySpan<byte> bytes, Encoding? encoding)
@@ -44,6 +75,7 @@ namespace Benchmarks.StringExtensions {
 			yield return new object? [] { Enumerable.Range (0, 1000).Select (i => (byte)i).ToArray (), null };
 		}
 
+		// TODO: Custom IParam for List<T> to display it properly in the benchmark summary.
 		public IEnumerable<object? []> ListDataSource ()
 		{
 			yield return new object? [] { new List<byte> (), null };
