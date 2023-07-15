b64c13167ea5946f21287b4eac1dbb777ae7aab1
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-07-15 11:23:04 +0300

    Use rented rune buffer array in TextFormatter.Draw

---
+++ Terminal.Gui/Text/ReadOnlySpanRuneExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Terminal.Gui {
+	/// <summary>
+	/// Extensions to <see cref="ReadOnlySpan{Rune}"/> to support TUI text manipulation.
+	/// </summary>
+	public static class ReadOnlySpanRuneExtensions {
+		/// <summary>
+		/// Gets the number of columns the runes occupy in the terminal.
+		/// </summary>
+		/// <remarks>
+		/// This is a Terminal.Gui extension method to <see cref="ReadOnlySpan{Rune}"/> to support TUI text manipulation.
+		/// </remarks>
+		/// <param name="runes">The runes to measure.</param>
+		/// <returns></returns>
+		public static int GetColumns (this ReadOnlySpan<Rune> runes)
+		{
+			int sum = 0;
+			foreach (var rune in runes) {
+				sum += Math.Max (rune.GetColumns (), 0);
+			}
+			return sum;
+		}
+
+		/// <summary>
+		/// Gets the number of columns the runes occupy in the terminal.
+		/// </summary>
+		/// <remarks>
+		/// This is a Terminal.Gui extension method to <see cref="Span{Rune}"/> to support TUI text manipulation.
+		/// </remarks>
+		/// <param name="runes">The runes to measure.</param>
+		/// <returns></returns>
+		public static int GetColumns (this Span<Rune> runes)
+		{
+			// This method exists just to avoid need to explicitly cast to ReadOnlySpan<Rune>.
+			return GetColumns ((ReadOnlySpan<Rune>)runes);
+		}
+	}
+}
